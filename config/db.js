cd042fe198bc76e1e3b44e4e1d8a4880b5d22860
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-29 00:18:00 +0800

    deploy to heroku

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      //   useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
+    // const conn = await mongoose.connect(process.env.MONGO_URI, {
+    //   useNewUrlParser: true,
+    //   //   useCreateIndex: true,
+    //   useUnifiedTopology: true,
+    // })
+    const conn = await mongoose.connect(process.env.MONGO_URI)
 
     console.log(
       `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
